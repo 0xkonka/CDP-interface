4895ead6c6bebefb85988d8cd5f6bf04ff462376
letteldream <letteldream@gmail.com>
2024-02-16 09:05:25 -0500

    chore: fetch Marketinfo

--- src/configs/address.ts
+++ src/configs/address.ts
@@ -7,27 +7,27 @@ export enum SupportedChainId {
 
 type AddressMap = { [chainId: number]: string }
 
-export const TREN_USD: AddressMap = {
+export const TREN_USD_ADDR: AddressMap = {
     [SupportedChainId.MAINNET]: '',
     [SupportedChainId.TESTNET]: '0xB5961a63a480032cF44cEE69f35Fd5042F6Cb5DF'
 }
 
-export const TREN_BOX: AddressMap = {
+export const TREN_BOX_ADDR: AddressMap = {
     [SupportedChainId.MAINNET]: '',
     [SupportedChainId.TESTNET]: '0x04F95712aB38299D07a37dF0b66f224E7474D613'
 }
 
-export const TREN_MARKET: AddressMap = {
+export const TREN_MARKET_ADDR: AddressMap = {
     [SupportedChainId.MAINNET]: '',
     [SupportedChainId.TESTNET]: '0xa8cE869AAeA6b09f05eaA4Ca4f4704517dC6c0f8'
 }
 
-export const PRICE_ORACLE: AddressMap = {
+export const PRICE_ORACLE_ADDR: AddressMap = {
     [SupportedChainId.MAINNET]: '',
     [SupportedChainId.TESTNET]: '0x45eD94a18853097adc5E1e8c3c3A9d64b1407e21'
 }
 
-export const MARKET_LENS: AddressMap = {
+export const MARKET_LENS_ADDR: AddressMap = {
     [SupportedChainId.MAINNET]: '',
-    [SupportedChainId.TESTNET]: '0x2daF4049d290A2D0413e6697F99c03C09caA1243”'
+    [SupportedChainId.TESTNET]: '0x2daF4049d290A2D0413e6697F99c03C09caA1243'
 }
