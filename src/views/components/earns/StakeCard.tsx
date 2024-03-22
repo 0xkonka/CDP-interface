ba34a5e0ce121a30ececdc2252fee8d8cc1a9e59
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-03-21 23:11:19 -0700

    Add sortable header component

--- src/views/components/earns/StakeCard.tsx
+++ src/views/components/earns/StakeCard.tsx
@@ -4,22 +4,30 @@ import { Stack, Typography, Button, Link } from '@mui/material'
 import CustomChip from '@/@core/components/mui/chip'
 import { Copy } from '../Copy';
 
-export const StakeCard = () => {
+interface Props {
+    balanceUSD: number
+    poolShare: number
+    txHash: string
+}
+
+export const StakeCard = (props: Props) => {
+    const {balanceUSD, poolShare, txHash} = props
+
     return (
         <Stack direction='column' alignItems='center' px={2.5} py={6} borderRadius={1.25} border='solid 1px #FFFFFF33'>
             <CustomChip label='Staked' color='primary' variant='filled' sx={{px: 3, mb: 6}}/>
-            <Typography fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`} variant='h2' color='primary' mb={4}>400 trenUSD</Typography>
-            <Typography color='#98999D' mb={6}>~ $400USD ~ Pool share 2%</Typography>
+            <Typography fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`} variant='h2' color='primary' mb={4}>{balanceUSD} trenUSD</Typography>
+            <Typography color='#98999D' mb={6}>~ ${balanceUSD}USD ~ Pool share {poolShare}%</Typography>
             <Button variant='outlined' color='primary' sx={{maxWidth: '100%', width: 450, py: {xs: 2, lg: 3}, color: 'white', mb: 6, fontSize: {xs: 16, md: 18}, fontWeight: 400, borderRadius: '10px'}}>
                 Withdraw
             </Button>
             <Stack direction='row' alignItems='center' justifyContent='space-between' pt={6} borderTop='solid 1px #C6C6C74D' sx={{maxWidth: '100%', width: 450}}>
-                <Link href='https://sepolia.etherscan.io/tx/0x1c36dc0dc64bf5e690f021464c2b5ca86d7e6ffcf5a0cb95e04d3adf4a3b3fbe' target="_blank" maxWidth='90%'>
+                <Link href={txHash} target="_blank" maxWidth='90%'>
                     <Typography fontWeight={300} sx={{maxWidth: '100%', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', textDecoration: 'underline', textDecorationColor: 'white'}}>
-                        https://sepolia.etherscan.io/tx/0x1c36dc0dc64bf5e690f021464c2b5ca86d7e6ffcf5a0cb95e04d3adf4a3b3fbe
+                        {txHash}
                     </Typography>
                 </Link>
-                <Copy text='https://sepolia.etherscan.io/tx/0x1c36dc0dc64bf5e690f021464c2b5ca86d7e6ffcf5a0cb95e04d3adf4a3b3fbe' />
+                <Copy text={txHash} />
             </Stack>
         </Stack>
     )
