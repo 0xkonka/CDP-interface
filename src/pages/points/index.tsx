8309cac03fe86f306b7eef2c3e48206fb3f5569b
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-03-27 16:56:53 -0700

    Remove animation when switching for ApexChart

--- src/pages/points/index.tsx
+++ src/pages/points/index.tsx
@@ -6,11 +6,11 @@ import { ActiveTask } from '@/views/components/points/ActiveTask'
 import { useGlobalValues } from '@/context/GlobalContext'
 
 const Points = () => {
-    const {isSmallScreen} = useGlobalValues()
+    const {isSmallScreen, isMediumScreen} = useGlobalValues()
 
     return (
         <Box>
-            <Box mt={10} position='relative' borderRadius={2.5} border='solid 1px #2D3131' overflow='hidden' sx={{px: 15, py: 9}}>
+            <Box mt={10} position='relative' borderRadius={2.5} border='solid 1px #2D3131' overflow='hidden' sx={{px: {xs: 4, md: 15}, py: {xs: 4, md: 9}}}>
                 <Box className="content" zIndex={10} position='relative'>
                     <Typography className='header-gradient' variant='h1'sx={{
                             mb: { xs: 4, md: 8 }, mt: 4,
@@ -20,15 +20,69 @@ const Points = () => {
                         Tren XP
                     </Typography>
                     <Typography variant={isSmallScreen ? 'subtitle1' : 'h5'} color='#F3F3F3'
-                        sx={{ fontWeight: 300, width: 730, maxWidth: '100%', lineHeight: { xs: 1.25, sm: 1.7 } }}>
+                        sx={{ fontWeight: 300, width: isSmallScreen ? 1 : 1/2, lineHeight: { xs: 1.25, sm: 1.7 } }}>
                         Deposit your collateral tokens into a module in exchange for a trenUSD loan or Loop 
                         your assets in one click to leverage exposure for your spot assets. Pay back your loan later using trenUSD or your collateral.
                     </Typography>
-                    <Stack direction='row' justifyContent='space-between' alignItems='center' mt={20}>
+                    <Box sx={{backgroundColor: {xs: '#0C0E0F', md: 'transparent'}}} paddingX={2}>
+                        <Grid container mt={20} spacing={4} alignItems='center'>
+                            <Grid item xs={6} lg={2.3}>
+                                <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>Total XP Gained</Typography>
+                                <Typography variant='h1'sx={{
+                                        fontSize: { xs: 16, md: 32, xl: 46 },
+                                    }}
+                                    fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                                >
+                                    232.2 XP
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} lg={2.3}>
+                                <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>XP Gained / 24 hrs </Typography>
+                                <Typography variant='h1'sx={{
+                                        fontSize: { xs: 16, md: 32, xl: 46 },
+                                    }}
+                                    fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                                >
+                                    24.1
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} lg={2.3}>
+                                <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>Rank</Typography>
+                                <Typography variant='h1'sx={{
+                                        fontSize: { xs: 16, md: 32, xl: 46 },
+                                    }}
+                                    fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                                >
+                                    #1,321
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} lg={2.3}>
+                                <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>Referrals</Typography>
+                                    <Typography variant='h1'sx={{
+                                            fontSize: { xs: 16, md: 32, xl: 46 },
+                                        }}
+                                        fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                                    >
+                                        50 XP
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} lg={2.8}>
+                                <Stack gap={2} width='fit-content' justifyContent='flex-end'>
+                                    <Stack direction='row' justifyContent='space-between'>
+                                        <Typography variant='h4' fontWeight={700}>Next Update</Typography>
+                                        <Typography variant='h4' fontWeight={700} color='primary'>08:51:58</Typography>
+                                    </Stack>
+                                    <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400}>Last Update: 3/3/2024 - 22:19:19</Typography>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                    
+                    {/* <Stack direction='row' justifyContent='space-between' alignItems='center' mt={20} sx={{backgroundColor: {xs: '#0C0E0F', md: 'transparent'}}}>
                         <Box>
-                            <Typography variant='h5' fontWeight={400} color='#C6C6C799'>Total XP Gained</Typography>
+                            <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>Total XP Gained</Typography>
                             <Typography variant='h1'sx={{
-                                    fontSize: { xs: 24, md: 32, xl: 46 },
+                                    fontSize: { xs: 16, md: 32, xl: 46 },
                                 }}
                                 fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
                             >
@@ -36,9 +90,9 @@ const Points = () => {
                             </Typography>
                         </Box>
                         <Box>
-                            <Typography variant='h5' fontWeight={400} color='#C6C6C799'>XP Gained / 24 hrs </Typography>
+                            <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>XP Gained / 24 hrs </Typography>
                             <Typography variant='h1'sx={{
-                                    fontSize: { xs: 24, md: 32, xl: 46 },
+                                    fontSize: { xs: 16, md: 32, xl: 46 },
                                 }}
                                 fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
                             >
@@ -46,9 +100,9 @@ const Points = () => {
                             </Typography>
                         </Box>
                         <Box>
-                            <Typography variant='h5' fontWeight={400} color='#C6C6C799'>Rank</Typography>
+                            <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>Rank</Typography>
                             <Typography variant='h1'sx={{
-                                    fontSize: { xs: 24, md: 32, xl: 46 },
+                                    fontSize: { xs: 16, md: 32, xl: 46 },
                                 }}
                                 fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
                             >
@@ -56,9 +110,9 @@ const Points = () => {
                             </Typography>
                         </Box>
                         <Box>
-                            <Typography variant='h5' fontWeight={400} color='#C6C6C799'>Referrals</Typography>
+                            <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>Referrals</Typography>
                             <Typography variant='h1'sx={{
-                                    fontSize: { xs: 24, md: 32, xl: 46 },
+                                    fontSize: { xs: 16, md: 32, xl: 46 },
                                 }}
                                 fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
                             >
@@ -70,9 +124,9 @@ const Points = () => {
                                 <Typography variant='h4' fontWeight={700}>Next Update</Typography>
                                 <Typography variant='h4' fontWeight={700} color='primary'>08:51:58</Typography>
                             </Stack>
-                            <Typography variant='h5' fontWeight={400}>Last Update: 3/3/2024 - 22:19:19</Typography>
+                            <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400}>Last Update: 3/3/2024 - 22:19:19</Typography>
                         </Stack>
-                    </Stack>
+                    </Stack> */}
                 </Box>
                 
                 <video autoPlay muted loop id="background-video" style={{ width: '100%', position: 'absolute', zIndex: '1', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
