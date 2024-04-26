3bdf18042beac2425323bf6d99acc37d4f1c5623
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-04-25 20:45:49 -0700

    Fix withdrawl available issue in popup

---
+++ src/pages/testnet/index.tsx
@@ -0,0 +1,32 @@
+import {Box} from '@mui/material'
+
+const Testnet = () => {
+    return (
+        <Box>
+        </Box>
+        // <Box height='100vh' overflow='hidden'>
+        //     <Box
+        //         sx={{
+        //             display: { xs: 'none', lg: 'block' },
+        //             width: '100vw',
+        //             height: '100vh',
+        //             position: 'absolute',
+        //             zIndex: '1',
+        //             top: -200,
+        //             left: -50,
+        //             // overflow: 'hidden'
+        //         }}
+        //     >
+        //     <video autoPlay muted loop id='background-video' >
+        //         <source
+        //         src='/videos/testnet-video.mp4'
+        //         type='video/mp4'    
+        //         />
+        //         Your browser does not support the video tag.
+        //     </video>
+        //     </Box>
+        // </Box>
+    )
+}
+
+export default Testnet
--- src/views/components/popups/borrowPopup.tsx
+++ src/views/components/popups/borrowPopup.tsx
@@ -142,9 +142,7 @@ export const BorrowPopup = (props: Props) => {
         setAvailableBalance(+formatUnits(userCollateralBal, decimals))
         break
       case 'withdraw':
-        setAvailableBalance(+formatEther(depositedAmount))
-        //  Once @Alex R found the reason why 0.25 TST was available to withdraw, we can use below codeline.
-        // setAvailableBalance(+formatEther(depositedAmount) - (+formatEther(debtAmount) / +formatEther(LTV) / +formatEther(price)))
+        setAvailableBalance(+formatEther(depositedAmount) - (+formatEther(debtAmount + debtTokenGasCompensation) / +formatEther(LTV) / +formatEther(price)))
         break
       case 'borrow':
         setAvailableBalance(+formatEther(depositedAmount) * +formatEther(price) * +formatEther(LTV) - (+formatEther(debtAmount)))
