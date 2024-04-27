1d619e14cd9698ae5ff068868f0495ea38aee6d0
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-04-26 17:36:02 -0700

    Add testnet landing

--- src/pages/testnet/index.tsx
+++ src/pages/testnet/index.tsx
@@ -1,31 +1,131 @@
-import {Box} from '@mui/material'
+import { useGlobalValues } from '@/context/GlobalContext'
+import {Box, Typography, Stack, Button} from '@mui/material'
 
 const Testnet = () => {
+    const {isSmallScreen, isMediumScreen, isMediumLargeScreen} = useGlobalValues()
+    const gradientBgStyle = {
+        background: 'linear-gradient(90deg, rgba(52, 97, 81, 0.60) 6.56%, rgba(81, 150, 125, 0.00) 88.13%)',
+        WebkitBackdropFilter: 'blur(1.244215965270996px)', // for Safari
+        backdropFilter: 'blur(1.244215965270996px)' // standard property, may not work as inline-style
+    }
+
     return (
         <Box>
+            <Box className='content' zIndex={10} position='relative'>
+                <Typography className='header-gradient' variant='h1'sx={{
+                        mb: { xs: 4, md: 4 }, mt: 10,
+                        fontSize: { xs: 36, md: 64, xl: 72 }
+                    }}
+                >
+                    Enabling the Internet Bond
+                </Typography>
+                <Typography fontSize={24} mb={8}>
+                    Synthetic dollar and Internet Native Yield
+                </Typography>
+                <Box style={gradientBgStyle} sx={{py: 3, px: 8, width: 484}}>
+                    <Typography variant='h2' color='primary' fontWeight={600}>sUSDe APY 35.4%</Typography>
+                </Box>
+                <Stack direction={isSmallScreen ? 'column' : 'row'} gap={8} mt={32}>
+                    <Stack sx={{flex: {xs: 1, lg: 1}, border: 'solid 1px #67DAB1', px: 10, py: 6, alignItems: {xs: 'center', lg: 'start'}}}>
+                        <Typography className='header-gradient' variant='h1' sx={{
+                                mb: { xs: 4, md: 4 },
+                                lineHeight: 1,
+                                fontSize: { xs: 18, md: 28, xl: 40 }
+                            }}
+                        >
+                            Early Access
+                        </Typography>
+                        <Typography sx={{fontSize: {xs: 12, lg: 20}, mb: 4}}>
+                            Enter your Invite Code 
+                        </Typography>
+                        <Box height={90} mb={8} sx={{background: 'green', width: 1}}>
+                            
+                        </Box>
+                        <Button
+                            className='gradient-stroke-button'
+                            sx={{
+                                py: 3,
+                                px: 10,
+                                fontSize: 24,
+                                borderRadius: '10px'
+                            }}
+                            variant='outlined'
+                        >
+                        Enter Code
+                        </Button>
+                    </Stack>
+                    <Stack sx={{flex: {xs: 1, lg: 2.5}, border: 'solid 1px #67DAB1', px: 10, py: 6, alignItems: {xs: 'center', lg: 'start'}}}>
+                        <Typography className='header-gradient' variant='h1' sx={{
+                                mb: { xs: 10, md: 14 },
+                                lineHeight: 1,
+                                fontSize: { xs: 18, md: 28, xl: 40 }
+                            }}
+                        >
+                            Backed by
+                        </Typography>
+                        <Stack direction={isMediumLargeScreen ? 'column': 'row'} justifyContent='space-between' gap={9} width='100%'>
+                            <Stack gap={12}>
+                                <Stack direction='row' justifyContent={isMediumScreen ? 'center' : 'flex-start'} gap={5}>
+                                    <img src='/images/logos/dragonfly.png' alt='Dragonfly' height={36}/>
+                                    <Stack direction='row' alignItems='center' gap={3.5}>
+                                        <img src='/images/avatars/arthur-hayes.png' width={32} alt='Arthur Hayes'/>
+                                        <Typography sx={{fontSize: {xs: 16, lg: 28}}}>Arthur Hayes</Typography>
+                                    </Stack>
+                                </Stack>
+                                <Stack direction='row' justifyContent='justify-between' gap={7.5}>
+                                    <img src='/images/logos/binance-labs.svg' alt='Binance Labs' height={26}/>
+                                    <img src='/images/logos/ventures.svg' alt='Ventures' height={26}/>
+                                    <img src='/images/logos/bybit.svg' alt='Bybit' height={26}/>
+                                    <img src='/images/logos/deribit.png' alt='Deribit' height={26}/>
+                                    <img src='/images/logos/mirana.png' alt='Mirana' height={26}/>
+                                </Stack>
+                            </Stack>
+                            <Stack direction='row' gap={4} sx={{justifyContent: {xs: 'space-between', lg: 'flex-start', xl: 'flex-end'}}}>
+                                <Stack direction='column' justifyContent='center' alignItems='center' gap={1}>
+                                    <img src='/images/avatars/synthetics.png' alt='Synthetics' width={50}/>
+                                    <Typography sx={{fontSize:{xs: 12, md: 14}}} mt={3}>SYNTHETICS</Typography>
+                                    <Typography sx={{fontSize:{xs: 12, md: 14}}} color='primary'>@kayinne</Typography>
+                                </Stack>
+                                <Stack direction='column' justifyContent='center' alignItems='center' gap={1}>
+                                    <img src='/images/avatars/aave.png' alt='Aave' width={50}/>
+                                    <Typography sx={{fontSize:{xs: 12, md: 14}}} mt={3}>AAVE</Typography>
+                                    <Typography sx={{fontSize:{xs: 12, md: 14}}} color='primary'>@Stanikulechov</Typography>
+                                </Stack>
+                                <Stack direction='column' justifyContent='center' alignItems='center' gap={1}>
+                                    <img src='/images/avatars/curve.png' alt='Curve' width={50}/>
+                                    <Typography sx={{fontSize:{xs: 12, md: 14}}} mt={3}>CURVE</Typography>
+                                    <Typography sx={{fontSize:{xs: 12, md: 14}}} color='primary'>@newmilwich</Typography>
+                                </Stack>
+                                <Stack direction='column' justifyContent='center' alignItems='center' gap={1}>
+                                    <img src='/images/avatars/frax.png' alt='Frax' width={50}/>
+                                    <Typography sx={{fontSize:{xs: 12, md: 14}}} mt={3}>FRAX</Typography>
+                                    <Typography sx={{fontSize:{xs: 12, md: 14}}} color='primary'>@samkazemian</Typography>
+                                </Stack>
+                            </Stack>
+                        </Stack>
+                    </Stack>
+                </Stack>
+            </Box>
+          
+            <Box sx={{
+                display: { xs: 'none', lg: 'block' },
+                width: '100vw',
+                height: 'calc(100vh + 170px)',
+                position: 'fixed',
+                zIndex: '1',
+                top: -170,
+                left: 0,
+                overflow: 'hidden'
+            }}>
+                <video autoPlay muted loop id='background-video' >
+                    <source
+                        src='/videos/testnet-video.mp4'
+                        type='video/mp4'    
+                    />
+                    Your browser does not support the video tag.
+                </video>
+            </Box>
         </Box>
-        // <Box height='100vh' overflow='hidden'>
-        //     <Box
-        //         sx={{
-        //             display: { xs: 'none', lg: 'block' },
-        //             width: '100vw',
-        //             height: '100vh',
-        //             position: 'absolute',
-        //             zIndex: '1',
-        //             top: -200,
-        //             left: -50,
-        //             // overflow: 'hidden'
-        //         }}
-        //     >
-        //     <video autoPlay muted loop id='background-video' >
-        //         <source
-        //         src='/videos/testnet-video.mp4'
-        //         type='video/mp4'    
-        //         />
-        //         Your browser does not support the video tag.
-        //     </video>
-        //     </Box>
-        // </Box>
     )
 }
 
--- src/views/components/points/TrenPointBanner.tsx
+++ src/views/components/points/TrenPointBanner.tsx
@@ -14,107 +14,107 @@ export const TrenPointBanner = () => {
             sx={{ px: { xs: 4, md: 8, xl: 15 }, py: { xs: 4, md: 9 } }}
         >
             <Stack className='content' zIndex={10} position='relative'>
-            <Box>
-                <Typography
-                className='header-gradient'
-                variant='h1'
-                sx={{
-                    mb: { xs: 4, md: 8 },
-                    mt: 4,
-                    fontSize: { xs: 36, md: 64, xl: 72 }
-                }}
-                >
-                Tren XP
-                </Typography>
-                <Typography
-                variant={isSmallScreen ? 'subtitle1' : 'h5'}
-                color='#F3F3F3'
-                sx={{ fontWeight: 300, width: isMediumScreen ? 1 : 1 / 2, lineHeight: { xs: 1.25, sm: 1.7 } }}
-                >
-                Deposit your collateral tokens into a module in exchange for a trenUSD loan or Loop your assets in one
-                click to leverage exposure for your spot assets. Pay back your loan later using trenUSD or your
-                collateral.
-                </Typography>
-            </Box>
-            <Box position='relative' sx={{ mt: { xs: 5, lg: 20 }, p: 4 }}>
-                <Grid container spacing={4} alignItems='center' ml={-8}>
-                <Grid item xs={6} lg={2.3}>
-                    <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>
-                    Total XP Gained
-                    </Typography>
+                <Box>
                     <Typography
+                    className='header-gradient'
                     variant='h1'
                     sx={{
-                        fontSize: { xs: 16, md: 32, xl: 46 },
-                        mt: { xs: 2, lg: 0 }
+                        mb: { xs: 4, md: 8 },
+                        mt: 4,
+                        fontSize: { xs: 36, md: 64, xl: 72 }
                     }}
-                    fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
                     >
-                    232.2 XP
-                    </Typography>
-                </Grid>
-                <Grid item xs={6} lg={2.3}>
-                    <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>
-                    XP Gained / 24 hrs{' '}
+                    Tren XP
                     </Typography>
                     <Typography
-                    variant='h1'
-                    sx={{
-                        fontSize: { xs: 16, md: 32, xl: 46 },
-                        mt: { xs: 2, lg: 0 }
-                    }}
-                    fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                    variant={isSmallScreen ? 'subtitle1' : 'h5'}
+                    color='#F3F3F3'
+                    sx={{ fontWeight: 300, width: isMediumScreen ? 1 : 1 / 2, lineHeight: { xs: 1.25, sm: 1.7 } }}
                     >
-                    24.1
+                    Deposit your collateral tokens into a module in exchange for a trenUSD loan or Loop your assets in one
+                    click to leverage exposure for your spot assets. Pay back your loan later using trenUSD or your
+                    collateral.
                     </Typography>
-                </Grid>
-                <Grid item xs={6} lg={2.3}>
-                    <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>
-                    Rank
-                    </Typography>
-                    <Typography
-                    variant='h1'
-                    sx={{
-                        fontSize: { xs: 16, md: 32, xl: 46 },
-                        mt: { xs: 2, lg: 0 }
-                    }}
-                    fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
-                    >
-                    #1,321
-                    </Typography>
-                </Grid>
-                <Grid item xs={6} lg={2.3}>
-                    <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>
-                    Referrals
-                    </Typography>
-                    <Typography
-                    variant='h1'
-                    sx={{
-                        fontSize: { xs: 16, md: 32, xl: 46 },
-                        mt: { xs: 2, lg: 0 }
-                    }}
-                    fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
-                    >
-                    50 XP
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} lg={2.8}>
-                    <Stack gap={2} width='fit-content' marginLeft={isMediumScreen ? 0 : 'auto'}>
-                    <Stack direction='row' justifyContent='space-between' gap={3}>
-                        <Typography variant='h4' fontWeight={700}>
-                        Next Update
+                </Box>
+                <Box position='relative' sx={{ mt: { xs: 5, lg: 20 }, p: 4 }}>
+                    <Grid container spacing={4} alignItems='center' ml={-8}>
+                    <Grid item xs={6} lg={2.3}>
+                        <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>
+                        Total XP Gained
                         </Typography>
-                        <Typography variant='h4' fontWeight={700} color='primary'>
-                        08:51:58
+                        <Typography
+                        variant='h1'
+                        sx={{
+                            fontSize: { xs: 16, md: 32, xl: 46 },
+                            mt: { xs: 2, lg: 0 }
+                        }}
+                        fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                        >
+                        232.2 XP
                         </Typography>
-                    </Stack>
-                    <Typography sx={{ fontSize: { xs: 14, md: 16, xl: 18 } }} fontWeight={400}>
-                        Last Update: 3/3/2024 - 22:19:19
-                    </Typography>
-                    </Stack>
-                </Grid>
-                </Grid>
-            </Box>
+                    </Grid>
+                    <Grid item xs={6} lg={2.3}>
+                        <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>
+                        XP Gained / 24 hrs{' '}
+                        </Typography>
+                        <Typography
+                        variant='h1'
+                        sx={{
+                            fontSize: { xs: 16, md: 32, xl: 46 },
+                            mt: { xs: 2, lg: 0 }
+                        }}
+                        fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                        >
+                        24.1
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} lg={2.3}>
+                        <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>
+                        Rank
+                        </Typography>
+                        <Typography
+                        variant='h1'
+                        sx={{
+                            fontSize: { xs: 16, md: 32, xl: 46 },
+                            mt: { xs: 2, lg: 0 }
+                        }}
+                        fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                        >
+                        #1,321
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} lg={2.3}>
+                        <Typography variant={isSmallScreen ? 'subtitle2' : 'h5'} fontWeight={400} color='#C6C6C799'>
+                        Referrals
+                        </Typography>
+                        <Typography
+                        variant='h1'
+                        sx={{
+                            fontSize: { xs: 16, md: 32, xl: 46 },
+                            mt: { xs: 2, lg: 0 }
+                        }}
+                        fontFamily={`'Britanica-HeavySemiExpanded', sans-serif`}
+                        >
+                        50 XP
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} lg={2.8}>
+                        <Stack gap={2} width='fit-content' marginLeft={isMediumScreen ? 0 : 'auto'}>
+                        <Stack direction='row' justifyContent='space-between' gap={3}>
+                            <Typography variant='h4' fontWeight={700}>
+                            Next Update
+                            </Typography>
+                            <Typography variant='h4' fontWeight={700} color='primary'>
+                            08:51:58
+                            </Typography>
+                        </Stack>
+                        <Typography sx={{ fontSize: { xs: 14, md: 16, xl: 18 } }} fontWeight={400}>
+                            Last Update: 3/3/2024 - 22:19:19
+                        </Typography>
+                        </Stack>
+                    </Grid>
+                    </Grid>
+                </Box>
             </Stack>
             <Box
             sx={{
