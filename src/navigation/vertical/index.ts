f1e977cc716215bd07a2c4ca5ae68e77d90ec9c7
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-06-28 22:37:58 +0400

    Test vercel deploy

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -24,7 +24,7 @@ const navigation = (): VerticalNavItemsType => {
       path: '/points'
     },
 
-   // =========== These are for testnet only ========== //
+   // ============ These are for testnet only =========== //
     // {
     //   title: 'Referrals',
     //   icon: 'referrals',
