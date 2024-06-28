ca37a16ab7cb92c448835d126162cf1c461c59b1
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-06-28 22:04:57 +0400

    Add all menus

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -3,11 +3,11 @@ import { VerticalNavItemsType } from 'src/@core/layouts/types'
 
 const navigation = (): VerticalNavItemsType => {
   return [
-    // {
-    //   title: 'Dashboard',
-    //   icon: 'dashboard',
-    //   path: '/dashboard'
-    // },
+    {
+      title: 'Dashboard',
+      icon: 'dashboard',
+      path: '/dashboard'
+    },
     {
       title: 'Modules',
       icon: 'modules',
@@ -18,44 +18,47 @@ const navigation = (): VerticalNavItemsType => {
       icon: 'earn',
       path: '/earn'
     },
-    // {
-    //   title: 'Points',
-    //   icon: 'points',
-    //   path: '/points'
-    // },
     {
-      title: 'Referrals',
-      icon: 'referrals',
-      path: '/referrals'
+      title: 'Points',
+      icon: 'points',
+      path: '/points'
+    },
+
+   // =========== These are for testnet only ========== //
+    // {
+    //   title: 'Referrals',
+    //   icon: 'referrals',
+    //   path: '/referrals'
+    // },
+    // {
+    //   title: 'Faucet',
+    //   icon: 'faucet',
+    //   path: '/faucet'
+    // }
+
+    {
+      title: 'Swap',
+      icon: 'swap',
+      path: '/swap'
     },
     {
-      title: 'Faucet',
-      icon: 'faucet',
-      path: '/faucet'
-    }
-    // {
-    //   title: 'Swap',
-    //   icon: 'swap',
-    //   path: '/swap'
-    // },
-    // {
-    //   title: 'Analytics',
-    //   icon: 'analytics',
-    //   children: [
-    //     {
-    //       title: 'Analytics 1',
-    //       path: '/analytics/analytics-1'
-    //     },
-    //     {
-    //       title: 'Analytics 2',
-    //       path: '/analytics/analytics-2'
-    //     },
-    //     {
-    //       title: 'Analytics 3',
-    //       path: '/analytics/analytics-3'
-    //     },
-    //   ]
-    // },
+      title: 'Analytics',
+      icon: 'analytics',
+      children: [
+        {
+          title: 'Analytics 1',
+          path: '/analytics/analytics-1'
+        },
+        {
+          title: 'Analytics 2',
+          path: '/analytics/analytics-2'
+        },
+        {
+          title: 'Analytics 3',
+          path: '/analytics/analytics-3'
+        },
+      ]
+    },
   ]
 }
 
